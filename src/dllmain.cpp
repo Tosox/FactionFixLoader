6a0175ed0275d6527de2681ca175b2b649ce0d25
Tosox <57193602+Tosox@users.noreply.github.com>
2026-03-16 18:05:17 +0100

    feat: Bump SDK version

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -25,7 +25,7 @@ extern "C" {
     }
 
     __declspec(dllexport) const char* GetModVersion() {
-        return "1.1.0";
+        return "1.2.0";
     }
 
     __declspec(dllexport) const char* GetModAuthor() {
